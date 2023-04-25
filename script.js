7deb179ad0685649b774c99587f0d933d88c8980
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 12:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function threeSum(arr, target) {
 // write your code here
-  
+    nums.sort((a, b) => a - b);
+    let closestSum = Infinity; 
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1; 
+        let right = nums.length - 1; 
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right]; 
+            if (Math.abs(sum - target) < Math.abs(closestSum - target))
+                closestSum = sum;
+            }
+            if (sum < target) {
+                left++;
+            } 
+			else if (sum > target) {
+                right--;
+            } 
+			else {
+                return target;
+            }
+        }
+    }
+	return closestSum;
 }
 
 module.exports = threeSum;
